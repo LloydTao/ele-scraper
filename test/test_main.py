ced8ddea9b62ea052484d1238fcc26f4307e5183
LloydTao <LL23@live.co.uk>
2020-05-04 12:16:46 +0100

    Create initial tests

---
+++ test/test_main.py
@@ -0,0 +1,14 @@
+def test_login():
+    assert False
+
+
+def test_get_course_resources():
+    assert False
+
+
+def test_get_resource():
+    assert False
+
+
+def test_create_driver():
+    assert False
